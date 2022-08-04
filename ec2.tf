5671053cbe6296279137d9c9721e58067e098031
Vdievbiere <vdievbiere@yahoo.com>
2022-08-04 00:00:42 -0400

    add new files

--- ec2.tf
+++ ec2.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "demoinstance1" {
   ami                         = "ami-087c17d1fe0178315"
   instance_type               = "t2.micro"
   count                       = 1
-  key_name                    = "tests"
+  key_name                    = "NovaRegionKey"
   vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
   subnet_id                   = "${aws_subnet.public-subnet-2.id}"
   associate_public_ip_address = true
